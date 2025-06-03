7a082e31b8976a523f783c007e2251bc0057fc26
ivanfelipevelascomedina <152944938+ivanfelipevelascomedina@users.noreply.github.com>
2025-06-03 03:13:18 +0200

    Update 4_Convergent_Thinking_Filtering .py

--- pages/4_Convergent_Thinking_Filtering .py
+++ pages/4_Convergent_Thinking_Filtering .py
@@ -58,8 +58,8 @@ if "roberta_tokenizer" not in st.session_state or "roberta_model" not in st.sess
     #tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
     #model = RobertaForMaskedLM.from_pretrained('roberta-base')
     # Downloads and caches the model
-    tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese")
-    model = AutoModelForMaskedLM.from_pretrained("cl-tohoku/bert-base-japanese")
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base-japanese-char-2epoch")
+    model = AutoModelForMaskedLM.from_pretrained("roberta-base-japanese-char-2epoch")
     model.eval()  # Set the model to evaluation mode
 
     # Store the model and tokenizer in session state
