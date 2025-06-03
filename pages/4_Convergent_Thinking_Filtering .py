0103c762ff854bdc91f65472730416fd61432f66
ivanfelipevelascomedina <152944938+ivanfelipevelascomedina@users.noreply.github.com>
2025-06-03 05:27:55 +0200

    Update 4_Convergent_Thinking_Filtering .py

--- pages/4_Convergent_Thinking_Filtering .py
+++ pages/4_Convergent_Thinking_Filtering .py
@@ -53,11 +53,11 @@ st.markdown("""
 # CHECK IF ROBERTA IS ALREADY LOADED IN THE SESSION STATE AND DO IT IN CASE IT IS NOT
 
 @st.cache_resource
-    def load_model():
-        tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
-        model = AutoModelForQuestionAnswering.from_pretrained("xlm-roberta-base")
-        #model.eval()  # Set the model to evaluation mode
-        return tokenizer, model
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+    model = AutoModelForQuestionAnswering.from_pretrained("xlm-roberta-base")
+    #model.eval()  # Set the model to evaluation mode
+    return tokenizer, model
 
 if "roberta_tokenizer" not in st.session_state or "roberta_model" not in st.session_state:
     st.write("RoBERTaモデルとトークナイザーを読み込んでいます。数秒かかる場合があります…")
